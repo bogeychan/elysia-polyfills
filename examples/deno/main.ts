2a06fdf367ee4cc904ffa7c06cf7033309cf969b
bogeychan <bogeychan@outlook.com>
2023-04-23 08:14:51 +0200

    update: examples

--- examples/deno/main.ts
+++ examples/deno/main.ts
@@ -6,20 +6,10 @@ import { Elysia } from 'elysia';
 // import 'npm:@sinclair/typebox@0.26.0';
 // import { Elysia } from 'npm:elysia@0.4.9';
 
-// const app = new Elysia()
-//   .get('/', () => ({ hello: 'Deno👋' }))
-//   .post('/:world', (ctx) => `Hello ${ctx.params.world}`)
-//   .listen(8080);
+const app = new Elysia()
+  .get('/', () => ({ hello: 'Deno👋' }))
+  .post('/:world', (ctx) => `Hello ${ctx.params.world}`)
+  .listen(8080);
 
-new Elysia()
-  .get('/', () => 'Hi')
-  .post('/json', (ctx) => ctx.body)
-  .get('/id/:id', (ctx) => {
-    ctx.set.headers['x-powered-by'] = 'benchmark';
-
-    return `${ctx.params.id} ${ctx.query.name}`;
-  })
-  .listen(3000);
-
-// console.log(`Listening on http://localhost:${app.server!.port}`);
+console.log(`Listening on http://localhost:${app.server!.port}`);
 
--- examples/node/main.ts
+++ examples/node/main.ts
@@ -2,15 +2,10 @@ import '@bogeychan/elysia-polyfills/node/index.js';
 
 import { Elysia } from 'elysia';
 
-new Elysia()
-  .get('/', () => 'Hi')
-  .post('/json', (ctx) => ctx.body)
-  .get('/id/:id', (ctx) => {
-    ctx.set.headers['x-powered-by'] = 'benchmark';
+const app = new Elysia()
+  .get('/', () => ({ hello: 'Node.js👋' }))
+  .post('/:world', (ctx) => `Hello ${ctx.params.world}`)
+  .listen(8080);
 
-    return `${ctx.params.id} ${ctx.query.name}`;
-  })
-  .listen(3000);
-
-// console.log(`Listening on http://localhost:${app.server!.port}`);
+console.log(`Listening on http://localhost:${app.server!.port}`);
 
